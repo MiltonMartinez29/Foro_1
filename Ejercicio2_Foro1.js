6337c51bb4db47ecc97fa01eaf3e053490682c32
MiltonMartinez29 <Alfredoxd321@gmail.com>
2023-04-25 23:10:30 -0600

    Ejercicio2_Foro1

---
+++ Ejercicio2_Foro1.js
@@ -0,0 +1,9 @@
+// Pedimos al usuario que ingrese un número
+let num = prompt("Ingrese un número: ");
+
+// Verificamos si el número es par o impar
+if(num % 2 == 0) {
+    console.log(num + " es par");
+} else {
+    console.log(num + " es impar");
+}
